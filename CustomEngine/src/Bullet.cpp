e8b407d452d5d121caa1a26158243c7052e00269
Hani Zaatar <hani.zaatar.92@gmail.com>
2023-02-16 17:35:59 +0100

    Pushing what should be the last classes of the CustomEngine first course

---
+++ CustomEngine/src/Bullet.cpp
@@ -0,0 +1,36 @@
+#include "Assets.h"
+#include "Bullet.h"
+#include "CircleCollisionComponent.h"
+#include "Game.h"
+#include "Enemy.h"
+#include "MoveComponent.h"
+#include "SpriteComponent.h"
+
+Bullet::Bullet()
+{
+	SpriteComponent* sc = new SpriteComponent(this, Assets::getTexture("Projectile"));
+	MoveComponent* mc = new MoveComponent(this);
+	mc->setForwardSpeed(400.0f);
+	circle = new CircleCollisionComponent(this);
+	circle->setRadius(5.0f);
+	lifetime = 1.0f;
+}
+
+void Bullet::updateActor(float dt)
+{
+	Actor::updateActor(dt);
+	for (Enemy* e : getGame().getEnemies())
+	{
+		if (Intersect(*circle, e->getCircle()))
+		{
+			// Both die on collision
+			e->setState(ActorState::Dead);
+			setState(ActorState::Dead);
+			break;
+		}
+	}
+
+	lifetime -= dt;
+	if (lifetime <= 0.0f)
+		setState(ActorState::Dead);
+}
---
+++ CustomEngine/src/Bullet.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Actor.h"
+
+class Bullet : public Actor
+{
+public:
+	Bullet();
+	void updateActor(float dt) override;
+
+private:
+	class CircleCollisionComponent* circle;
+	float lifetime;
+};
