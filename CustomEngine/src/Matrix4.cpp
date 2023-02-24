160f019d047e879bc4dbb5fcb34223bc5006d25c
Hani Zaatar <hani.zaatar.92@gmail.com>
2023-02-24 19:10:50 +0100

    Vector3 class and some of the Matrix4 class

---
+++ CustomEngine/src/Matrix4.cpp
@@ -0,0 +1,48 @@
+#include "Matrix4.h"
+
+static float m4Identity[4][4] =
+{
+	{1.0f, 0.0f, 0.0f, 0.0f},
+	{0.0f, 1.0f, 0.0f, 0.0f},
+	{0.0f, 0.0f, 1.0f, 0.0f},
+	{0.0f, 0.0f, 0.0f, 1.0f}
+};
+
+const Matrix4 Matrix4::identity(m4Identity);
+
+void Matrix4::invert()
+{
+	float tmp[12];
+	float src[16];
+	float dst[16];
+	float det;
+
+	// Transponse matrix
+	// row 1 to col 1
+	src[0] = mat[0][0];
+	src[4] = mat[0][1];
+	src[8] = mat[0][2];
+	src[12] = mat[0][3];
+
+	// row 2 to col 2
+	src[1] = mat[1][0];
+	src[5] = mat[1][1];
+	src[9] = mat[1][2];
+	src[13] = mat[1][3];
+
+	// row 3 to col 3
+	src[2] = mat[2][0];
+	src[6] = mat[2][1];
+	src[10] = mat[2][2];
+	src[14] = mat[2][3];
+
+	// row 4 to col 4
+	src[3] = mat[3][0];
+	src[7] = mat[3][1];
+	src[11] = mat[3][2];
+	src[15] = mat[3][3];
+
+	// Calculate cofactors
+	tmp[0] = src[10] * src[15];
+	tmp[1] = src[11] * src[14];
+}
--- CustomEngine/src/Vector3.h
+++ CustomEngine/src/Vector3.h
@@ -43,6 +43,11 @@ public:
 		return Vector3(vec.x * scalar, vec.y * scalar, vec.z * scalar);
 	}
 
+	friend Vector3 operator*(float scalar, const Vector3& vec)
+	{
+		return Vector3(vec.x * scalar, vec.y * scalar, vec.z * scalar);
+	}
+
 	// Scalar *=
 	Vector3& operator*=(float scalar)
 	{
@@ -51,4 +56,71 @@ public:
 		z *= scalar;
 		return *this;
 	}
+
+	// Vector +=
+	Vector3& operator+=(const Vector3& right)
+	{
+		x += right.x;
+		y += right.y;
+		z += right.z;
+		return *this;
+	}
+
+	// Vector -=
+	Vector3& operator-=(const Vector3& right)
+	{
+		x -= right.x;
+		y -= right.y;
+		z -= right.z;
+		return *this;
+	}
+
+	// Normalize the provided vector
+	static Vector3 normalize(const Vector3& vec)
+	{
+		Vector3 temp = vec;
+		temp.normalize();
+		return temp;
+	}
+
+	// Dot product between two vectors (a dot b)
+	static float dot(const Vector3& a, const Vector3& b)
+	{
+		return (a.x * b.x + a.y * b.y + a.z * b.z);
+	}
+
+	// Cross product between two vectors (a cross b)
+	static Vector3 cross(const Vector3& a, const Vector3& b)
+	{
+		Vector3 temp;
+		temp.x = a.y * b.z - a.z * b.y;
+		temp.y = a.z * b.x - a.x * b.z;
+		temp.z = a.x * b.y - a.y * b.x;
+		return temp;
+	}
+
+	// Lerp from A to B by f
+	static Vector3 lerp(const Vector3& a, const Vector3& b, float f)
+	{
+		return Vector3(a + f * (b - a));
+	}
+
+	// Reflect V about (normalized) N
+	static Vector3 reflect(const Vector3& v, const Vector3& n)
+	{
+		return v - 2.0f * Vector3::dot(v, n) * n;
+	}
+
+	static const Vector3 zero;
+	
+	static const Vector3 unitX;
+	static const Vector3 unitY;
+	static const Vector3 unitZ;
+
+	static const Vector3 negUnitX;
+	static const Vector3 negUnitY;
+	static const Vector3 negUnitZ;
+
+	static const Vector3 infinity;
+	static const Vector3 negInfinity;
 };
